bc07113c5b7b13791ff1822b8215ac3dab7be2fd
ThePsyjo <ThePsyjo@googlemail.com>
2021-05-18 13:56:13 +0200

    disable hash routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     initialNavigation: 'enabled',
-    useHash: true,
+//    useHash: true,
 })],
   exports: [RouterModule]
 })
