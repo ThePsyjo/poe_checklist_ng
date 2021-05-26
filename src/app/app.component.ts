edd255231f6165041626a02d1e56c19c2b5989c6
ThePsyjo <ThePsyjo@googlemail.com>
2021-05-26 17:57:46 +0200

    add ad banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'poe-checklist-ng';
+
+  get ga(): Record<string, string | null> {
+    return 'ga' in environment ? environment['ga'] : {client: null, slot: null}
+  }
 }
