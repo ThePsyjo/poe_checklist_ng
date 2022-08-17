92552811a15aa5806001a8c6d33fdb407ef9f93e
ThePsyjo <ThePsyjo@googlemail.com>
2022-08-17 18:36:35 +0200

    upgrade to angular 14

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled',
+    initialNavigation: 'enabledBlocking',
 //    useHash: true,
 })],
   exports: [RouterModule]
