805808b6120621b17076619980b343ef373b50e5
ThePsyjo <ThePsyjo@googlemail.com>
2023-05-12 17:55:49 +0200

    set http-status in NotFoundComponent if possible

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit, Optional} from '@angular/core';
+import {RESPONSE} from "@nguniversal/express-engine/tokens";
+import {Response} from "express";
 
 @Component({
     selector: 'app-not-found',
@@ -8,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFoundComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Optional() @Inject(RESPONSE) private response: Response
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    if(this.response){
+      this.response.status(404);
+    }
   }
 
 }
