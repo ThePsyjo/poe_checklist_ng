158404dac5024fc52a9780c63226e63a50e722d6
ThePsyjo <ThePsyjo@googlemail.com>
2023-05-17 21:25:55 +0200

    remove remaining pantheon residue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import {RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
   {path: '', loadComponent: () => import('./maps/maps.component').then(c => c.MapsComponent)},
   {path: 'passives', loadComponent: () => import('./passives/passives.component').then(c => c.PassivesComponent)},
-  // {path: 'pantheons', loadComponent: () => import('./pantheons/pantheons.component').then(c => c.PantheonsComponent)},
   {path: 'trials', loadComponent: () => import('./trials/trials.component').then(c => c.TrialsComponent)},
   {path: 'about', loadComponent: () => import('./about/about.component').then(c => c.AboutComponent)},
   {path: '404', loadComponent: () => import('./not-found/not-found.component').then(c => c.NotFoundComponent)},
