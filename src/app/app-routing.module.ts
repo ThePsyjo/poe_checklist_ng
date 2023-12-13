51a68010ec776d54ce0124ae329d325df3e47279
ThePsyjo <ThePsyjo@googlemail.com>
2023-12-13 18:37:38 +0100

    up-trade: introduce component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   {path: '', loadComponent: () => import('./maps/maps.component').then(c => c.MapsComponent)},
   {path: 'passives', loadComponent: () => import('./passives/passives.component').then(c => c.PassivesComponent)},
   {path: 'trials', loadComponent: () => import('./trials/trials.component').then(c => c.TrialsComponent)},
+  {path: 'up-trade', loadComponent: () => import('./up-trade/up-trade.component').then(c => c.UpTradeComponent)},
   {path: 'about', loadComponent: () => import('./about/about.component').then(c => c.AboutComponent)},
   {path: '404', loadComponent: () => import('./not-found/not-found.component').then(c => c.NotFoundComponent)},
   {path: '**', redirectTo: '/404'},
---
+++ src/app/up-trade/up-trade.component.ts
@@ -0,0 +1,50 @@
+import {Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+
+interface Combination extends Record<string, any> {
+  number: number;
+  combination: string;
+}
+
+@Component({
+  selector: 'app-up-trade',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './up-trade.component.html',
+  styleUrl: './up-trade.component.css'
+})
+export class UpTradeComponent {
+  value = 4;  // default count
+
+  // has to be here to make wheel work
+  wheelSpun(event: Event): void {
+  }
+
+  calcCombinations(array: any[], size: number) {
+
+    function p(t: any[], i: number) {
+      if (t.length === size) {
+        result.push(t);
+        return;
+      }
+      if (i + 1 > array.length) {
+        return;
+      }
+      p(t.concat(array[i]), i + 1);
+      p(t, i + 1);
+    }
+
+    var result: any[] = [];
+    p([], 0);
+    return result;
+  }
+
+  * getCombinations(): Generator<Combination> {
+    let n = 1
+    for (const combination of this.calcCombinations(Array.from(Array(this.value).keys()).map(v => v + 1), 3)) {
+      yield {number: n++, combination: Array(...combination).join(' - ')};
+    }
+  }
+}
